6d5189c5ab730f46a24dafed58ae503486f77380
Timothée Gaultier <tiimogo@gmail.com>
2023-11-27 21:58:55 +0100

    improve lang

--- src/commands/init.cli.ts
+++ src/commands/init.cli.ts
@@ -2,14 +2,19 @@ import fs from 'fs';
 import path from 'path';
 import { FileManagementKeysEnum} from "../resources/global/translations.js";
 import {
+    DEFAULT_LOCALE,
+    getCurrentLang,
     getSystemLanguage,
-    getTranslation,
+    getTranslation, LanguageCode, setCurrentLang, setFullLanguageName,
     setLangToSystem,
 } from "../utils/language.utils.js"
 
 export const init = async () => {
     setLangToSystem(); // Définit la langue courante sur la langue système
     const currentLang = getSystemLanguage().langCode; // Récupère la langue système
+    const systemLanguage = process.env.LANG || currentLang;
+    const langCode = systemLanguage.includes(DEFAULT_LOCALE) ? LanguageCode.EN : LanguageCode.FR;
+
 
 
     const directoryName = "dist/.wizgen";
@@ -21,28 +26,37 @@ export const init = async () => {
 
     for (const file of files) {
         const filePath = path.join(directoryName, file);
-        let fileContent = "";
+        let fileContent: Object = "";
 
         switch (file) {
             case "settings.json":
+                let fullLanguageName = setFullLanguageName(currentLang);
                 const settings = {
                     lang: {
-                        fullLanguage: currentLang,
-                        langCode: currentLang,
+                        fullLanguage: fullLanguageName,
+                        langCode: langCode, // Utilisez "langCode" ici au lieu de "currentLang"
+                    },
+                    database: {
+                        driver: null,
+                        hostName: null,
+                        port: null,
+                        databaseName: null,
+                        userName: null,
+                        password: null,
                     }
                 };
                 fileContent = JSON.stringify(settings, null, 2);
                 break;
 
             case "entity.definition.json":
-                fileContent = "{}";
+                fileContent = `{}`
                 break;
 
             // Ajoutez d'autres cas si nécessaire
         }
 
         try {
-            fs.writeFileSync(filePath, fileContent);
+            fs.writeFileSync(filePath, fileContent as string);
             const successMessageKey = FileManagementKeysEnum.FILE_CREATED;
             const successMessage = getTranslation(currentLang, "file_management", successMessageKey as string);
 
--- src/utils/language.utils.ts
+++ src/utils/language.utils.ts
@@ -1,151 +1,154 @@
-  import {
-    ErrorMessagesEnum,
-    FileManagementKeysEnum,
-    QuestionsKeysEnum,
-    translations
-  } from "../resources/global/translations.js";
+// language.utils.ts
+import {
+  ErrorMessagesEnum,
+  FileManagementKeysEnum,
+  QuestionsKeysEnum,
+  translations
+} from "../resources/global/translations.js";
 
+// A language enum based on SystemLanguageInfo
+export enum LanguageCode {
+  EN = "en_US",
+  FR = "fr_FR",
+}
 
+export enum FullLanguageUIName {
+  EN = "English",
+  FR = "Français",
+}
 
-  // A language enum based on SystemLanguageInfo
-  export enum LanguageCode {
-    EN = "en_US",
-    FR = "fr_FR",
+export type LanguageKeys = keyof typeof LanguageCode;
+
+export const LANGUAGES: LanguageCode[] = [LanguageCode.EN, LanguageCode.FR];
+export const ALL_FULL_LANGUAGE_UI_NAMES = Object.values(FullLanguageUIName) as FullLanguageUIName[];
+
+export interface SystemLanguageInfo {
+  fullLanguage: FullLanguageUIName;
+  langCode: LanguageCode;
+}
+
+export enum Separator {
+  UNDERSCORE = "_",
+  DASH = "-",
+}
+
+export const DEFAULT_LOCALE: LanguageCode = 'en_US' as LanguageCode;
+const LOCALE_FR: LanguageCode = 'fr_FR' as LanguageCode;
+
+let currentLang: LanguageCode;
+
+export const getSystemLanguage = (): SystemLanguageInfo => {
+  const systemLanguage = process.env.LANG || DEFAULT_LOCALE;
+  const langCode = systemLanguage.includes(LOCALE_FR) ? LanguageCode.FR : LanguageCode.EN;
+
+  let fullLanguageName: FullLanguageUIName;
+  switch (langCode) {
+    case LanguageCode.EN:
+      fullLanguageName = FullLanguageUIName.EN;
+      break;
+    case LanguageCode.FR:
+      fullLanguageName = FullLanguageUIName.FR;
+      break;
+    default:
+      fullLanguageName = FullLanguageUIName.EN;
+      break;
   }
-  export enum FullLanguageUIName {
-      EN = "English",
-      FR = "Français",
-  }
-  export type LanguageKeys = keyof typeof LanguageCode;
 
-  // Liste de toutes les langues supportées par la CLI dans un tableau
-  export const LANGUAGES: LanguageCode[] = [LanguageCode.EN, LanguageCode.FR];
-  export const ALL_FULL_LANGUAGE_UI_NAMES = Object.values(FullLanguageUIName) as FullLanguageUIName[];
-
-  export interface SystemLanguageInfo {
-    fullLanguage: FullLanguageUIName;
-    langCode: LanguageCode;
-  }
-
-  export enum Separator {
-    UNDERSCORE = "_",
-    DASH = "-",
-  }
-
-  // Constantes pour les chaînes codées en dur
-  const DEFAULT_LOCALE: LanguageCode = 'en_US' as LanguageCode;
-  const LOCALE_FR: LanguageCode = 'fr_FR' as LanguageCode;
-
-  export const getSystemLanguage = (): SystemLanguageInfo => {
-    const systemLanguage = process.env.LANG || DEFAULT_LOCALE;
-    const langCode = systemLanguage.includes(LOCALE_FR) ? LanguageCode.FR : LanguageCode.EN;
-
-    // Détermine le nom complet de la langue pour l'interface utilisateur
-    let fullLanguageName: FullLanguageUIName;
-    switch (langCode) {
-      case LanguageCode.EN:
-        fullLanguageName = FullLanguageUIName.EN;
-        break;
-      case LanguageCode.FR:
-        fullLanguageName = FullLanguageUIName.FR;
-        break;
-      default:
-        fullLanguageName = FullLanguageUIName.EN; // Valeur par défaut
-        break;
-    }
-
-    return {
-      fullLanguage: fullLanguageName,
-      langCode,
-    };
+  return {
+    fullLanguage: fullLanguageName,
+    langCode: langCode, // Utilisez "langCode" ici au lieu de "currentLang"
   };
-  export function setFullLanguageName(langCode: LanguageCode): FullLanguageUIName {
-    let fullLanguageName: FullLanguageUIName;
-    switch (langCode) {
-      case LanguageCode.EN:
-        fullLanguageName = FullLanguageUIName.EN;
-        break;
-      case LanguageCode.FR:
-        fullLanguageName = FullLanguageUIName.FR;
-        break;
-      default:
-        fullLanguageName = FullLanguageUIName.EN; // Valeur par défaut
-        break;
-    }
-    return fullLanguageName;
+};
+
+
+currentLang = getSystemLanguage().langCode; // Initialise currentLang ici
+
+export function setFullLanguageName(langCode: LanguageCode): FullLanguageUIName {
+  let fullLanguageName: FullLanguageUIName;
+  switch (langCode) {
+    case LanguageCode.EN:
+      fullLanguageName = FullLanguageUIName.EN;
+      break;
+    case LanguageCode.FR:
+      fullLanguageName = FullLanguageUIName.FR;
+      break;
+    default:
+      fullLanguageName = FullLanguageUIName.EN;
+      break;
   }
+  return fullLanguageName;
+}
 
-  export type Questions = Record<string, string>;
-  export type ErrorMessages = Record<string, string>;
-  export type FileManagement = Record<string, string>;
+export type Questions = Record<string, string>;
+export type ErrorMessages = Record<string, string>;
+export type FileManagement = Record<string, string>;
 
-  interface Resources {
-    [key: string]: Record<string, string>;
-  }
+interface Resources {
+  [key: string]: Record<string, string>;
+}
 
-  interface Resource {
-    [key: string]: string;
-  }
+interface Resource {
+  [key: string]: string;
+}
 
-  // Ici, vous devriez remplir les ressources linguistiques pour chaque langue.
-  const LANGUAGE_RESOURCES: Record<LanguageCode, Resources> = {
-    [LanguageCode.EN]: {
-      QUESTIONS: {
-        ...QuestionsKeysEnum, // Intégration des questions
-        // Vos questions en anglais
-      },
-      ERROR_MESSAGES: {
-        ...ErrorMessagesEnum, // Intégration des messages d'erreur
-        // Autres messages d'erreur spécifiques en anglais
-      },
-      FILE_MANAGEMENT: {
-        ...FileManagementKeysEnum, // Intégration des messages de gestion de fichiers
-        // Autres messages de gestion de fichiers spécifiques en anglais
-      },
+const LANGUAGE_RESOURCES: Record<LanguageCode, Resources> = {
+  [LanguageCode.EN]: {
+    QUESTIONS: {
+      ...QuestionsKeysEnum,
+      // Vos questions en anglais
     },
-    [LanguageCode.FR]: {
-      QUESTIONS: {
-        // Vos questions en français
-      },
-      ERROR_MESSAGES: {
-        ...QuestionsKeysEnum, // Traduisez ces messages en français
-        // Autres messages d'erreur spécifiques en français
-      },
-      FILE_MANAGEMENT: {
-        ...FileManagementKeysEnum, // Traduisez ces messages en français
-        // Autres messages de gestion de fichiers spécifiques en français
-      },
+    ERROR_MESSAGES: {
+      ...ErrorMessagesEnum,
+      // Autres messages d'erreur spécifiques en anglais
     },
-  };
+    FILE_MANAGEMENT: {
+      ...FileManagementKeysEnum,
+      // Autres messages de gestion de fichiers spécifiques en anglais
+    },
+  },
+  [LanguageCode.FR]: {
+    QUESTIONS: {
+      // Vos questions en français
+    },
+    ERROR_MESSAGES: {
+      ...QuestionsKeysEnum,
+      // Autres messages d'erreur spécifiques en français
+    },
+    FILE_MANAGEMENT: {
+      ...FileManagementKeysEnum,
+      // Autres messages de gestion de fichiers spécifiques en français
+    },
+  },
+};
 
-  let currentLang: LanguageCode = getSystemLanguage().langCode;
+export const getCurrentLang = (): LanguageCode => {
+  return currentLang;
+};
 
-  export const getCurrentLang = (): LanguageCode => {
-    return currentLang;
-  };
-
-  export const setCurrentLang = (targetedLang: LanguageCode): void => {
+export const setCurrentLang = (targetedLang: LanguageCode): LanguageCode => {
     currentLang = targetedLang;
-  };
+    return currentLang;
+}
 
-  export const setLangToSystem = () => {
-    console.log(`Setting language to system language: ${getSystemLanguage().langCode}`);
-   return  currentLang = getSystemLanguage().langCode;
-  };
+export const setLangToSystem = () => {
+  console.log(`Setting language to system language: ${getSystemLanguage().langCode}`);
+  currentLang = getSystemLanguage().langCode;
+  console.log(`Language set to: ${getSystemLanguage().fullLanguage} (${getSystemLanguage().langCode})`);
 
-  // Exemple de vérification dans getTranslation
-  export function getTranslation(language: LanguageCode, resource: keyof typeof translations[LanguageCode], key: string): string {
-    const resourceTranslations = translations[language][resource] as Record<string, string>;
-    const translation = resourceTranslations[key];
-    if (translation === undefined) {
-      throw new Error(`Translation key not found: ${key} in resource: ${resource} for language: ${language}`);
-    }
-    return translation;
+};
+
+export function getTranslation(language: LanguageCode, resource: keyof typeof translations[LanguageCode], key: string): string {
+  const resourceTranslations = translations[language][resource] as Record<string, string>;
+  const translation = resourceTranslations[key];
+  if (translation === undefined) {
+    throw new Error(`Translation key not found: ${key} in resource: ${resource} for language: ${language}`);
   }
-  export enum StringType {
-    CamelCase,
-    CapitalizedCase,
-    SpacesToUnderscores,
-    CamelCaseToUnderscores
-  }
+  return translation;
+}
 
+export enum StringType {
+  CamelCase,
+  CapitalizedCase,
+  SpacesToUnderscores,
+  CamelCaseToUnderscores
+}
